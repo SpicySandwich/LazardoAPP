0b40caf3dad5d687b8e598b781d110bc202987b9
Conred Alas-as <conredalasas12@gmail.com>
2022-08-15 01:22:38 +0800

    bit of changes

--- LazardoClient/src/main/java/com/client/LazardoClient/DAO/ClientBalance.java
+++ LazardoClient/src/main/java/com/client/LazardoClient/DAO/ClientBalance.java
@@ -11,35 +11,35 @@ import org.apache.ibatis.annotations.Update;
 public interface ClientBalance {
 	
 	final String ADD_BALANCE = "UPDATE tbclientdetails  cd "
-													+ "JOIN ( "
-													+ "SELECT cd2.money_balance AS currentbalance, cd2.client_email AS clientemail "
-													+ "FROM tbclientdetails cd2 "
-													+ "WHERE cd2.client_email = #{clientEmail} "
-													+ ") balance "
-													+ "SET cd.money_balance = balance.currentbalance + #{balance} "
-													+ "WHERE cd.client_email = balance.clientemail AND balance.currentbalance  <= 0 ;";
+				+ "JOIN ( "
+				+ "SELECT cd2.money_balance AS currentbalance, cd2.client_email AS clientemail "
+				+ "FROM tbclientdetails cd2 "
+				+ "WHERE cd2.client_email = #{clientEmail} "
+				+ ") balance "
+				+ "SET cd.money_balance = balance.currentbalance + #{balance} "
+				+ "WHERE cd.client_email = balance.clientemail AND balance.currentbalance  <= 0 ;";
 	
 	final String TRANSFER_BALANCE_PROCESS_1 = "UPDATE tbclientdetails  cd "
-													+ "JOIN( "
-													+ "SELECT cd2.client_email AS clientEmail, cd2.money_balance - #{balanceTransfer} AS transferbalance "
-													+ "FROM tbclientlogin cl2 "
-													+ "LEFT JOIN tbclientdetails cd2 "
-													+ "ON cl2.client_login_id = cd2.client_id "
-													+ "WHERE cd2.client_email  = #{senderEmail} "
-													+ ") AS transfer "
-													+ "SET cd.money_balance = if(transfer.transferbalance >= 0, transfer.transferbalance, cd.money_balance) "
-													+ "WHERE cd.client_email  = transfer.clientEmail AND transfer.transferbalance >= 0 ; ";
+				+ "JOIN( "
+				+ "SELECT cd2.client_email AS clientEmail, cd2.money_balance - #{balanceTransfer} AS transferbalance "
+				+ "FROM tbclientlogin cl2 "
+				+ "LEFT JOIN tbclientdetails cd2 "
+				+ "ON cl2.client_login_id = cd2.client_id "
+				+ "WHERE cd2.client_email  = #{senderEmail} "
+				+ ") AS transfer "
+				+ "SET cd.money_balance = if(transfer.transferbalance >= 0, transfer.transferbalance, cd.money_balance) "
+				+ "WHERE cd.client_email  = transfer.clientEmail AND transfer.transferbalance >= 0 ; ";
 
 	final String TRANSFER_BALANCE_PROCESS_2 = "UPDATE tbclientdetails  cd "
-												+ "JOIN( "
-												+ "SELECT cd2.client_email AS clientEmail, cd2.money_balance +  #{balanceTransfer} AS transferbalance "
-												+ "FROM tbclientlogin cl2 "
-												+ "LEFT JOIN tbclientdetails cd2 "
-												+ "ON cl2.client_login_id = cd2.client_id "
-												+ "WHERE cd2.client_email  = #{receiverEmail} "
-												+ ") AS transfer2 "
-												+ "SET cd.money_balance = if( transfer2.transferbalance >= 0, transfer2.transferbalance, cd.money_balance) "
-												+ "WHERE cd.client_email  = transfer2.clientEmail  AND transfer2.transferbalance >= 0;";
+				+ "JOIN( "
+				+ "SELECT cd2.client_email AS clientEmail, cd2.money_balance +  #{balanceTransfer} AS transferbalance "
+				+ "FROM tbclientlogin cl2 "
+				+ "LEFT JOIN tbclientdetails cd2 "
+				+ "ON cl2.client_login_id = cd2.client_id "
+				+ "WHERE cd2.client_email  = #{receiverEmail} "
+				+ ") AS transfer2 "
+				+ "SET cd.money_balance = if( transfer2.transferbalance >= 0, transfer2.transferbalance, cd.money_balance) "
+				+ "WHERE cd.client_email  = transfer2.clientEmail  AND transfer2.transferbalance >= 0;";
 	
 	@Update(ADD_BALANCE)
 	boolean addBalance( AddBalance addBalance);
